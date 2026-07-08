fecca4cf143133c636600f3e83829f4e84b8c23f
Shivaram <orugtechnologies@gmail.com>
2026-07-09 01:23:05 +0530

    fix qr linking issue

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ const client = new Client({
             '--no-zygote',
             '--disable-gpu'
         ]
+    },
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
     }
 });
 
