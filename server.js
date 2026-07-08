ca01ac3f38e9c0f583ed165f8ad8ad788045f287
Shivaram <orugtechnologies@gmail.com>
2026-07-09 01:03:09 +0530

    add Dockerfile to fix chromium on render

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ let isConnected = false;
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        executablePath: process.env.RENDER ? '/usr/bin/google-chrome' : undefined,
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ]
     }
 });
 
